0b3102658d133e73e1f280c6649c45a8cee1fcd7
AlexABA91 <80538104+AlexABA91@users.noreply.github.com>
2021-03-25 17:21:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(document).ready(function () {
+        $('.burger').click(function (event) {
+            $('.burger,.menu').toggleClass('active');
+        });
+    });
+});
+
