ac6489839a1ffd499b12a60efdded30219e692c8
AlexABA91 <80538104+AlexABA91@users.noreply.github.com>
2021-06-08 13:00:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ $(function () {
             $('.burger,.menu').toggleClass('active');
         });
     });
+
+    $('.slider').slick({
+    });
+
+    $('#tabs li').click(function () {
+        var thisClass = this.className.slice(0, 2);
+        $('div.t1').hide();
+        $('div.t2').hide();
+        $('div.t3').hide();
+        $('div.' + thisClass).fadeIn(500);
+        $('#tabs li').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    });
+    $('li.t1').click();
+
 });
 
